6e833dc0e618caa187a171de5f8bbf42b7b5c710
e-esquinas2020 <e.esquinas.2020@alumnos.urjc.es>
2021-02-17 17:58:15 +0100

    Debug 1

---
+++ sesion_03/debug_me.py
@@ -0,0 +1,12 @@
+def g(a, b):
+    return a - b
+
+def f(a, b, c, d):
+    t0 = a + b - g(a, 0)
+    t1 = g(c, d)
+    t3 = 2*(t0/t1)
+    return t0 + 2*t1 + t3*t3
+
+print("R1:", f(5, 2, 5, 0))
+print("R2:", f(0, 2, 3, 3))
+print("R3:", f(1, 3, 2, 3))
