1a796d0e2dca97ba46692268bac370964442f6d7
e-esquinas2020 <e.esquinas.2020@alumnos.urjc.es>
2021-02-24 17:27:45 +0100

    not working

---
+++ P0/Ex1.py
@@ -0,0 +1,3 @@
+import Seq0
+print("We are testing Seq0 module")
+Seq0.seq_ping()
--- sesion_03/dna_read.py
+++ sesion_03/dna_read.py
@@ -1,4 +1,3 @@
-
 def count_dna(seq):
     count_a, count_g, count_c, count_t = 0, 0, 0, 0
     for e in seq:
@@ -8,10 +7,15 @@ def count_dna(seq):
             count_g += 1
         elif str(e).lower == "t":
             count_t += 1
-        elif str(e).lower() == "c":
+        elif str(e).lower == "c":
             count_c += 1
     return count_a, count_g, count_c, count_t
 
-seq = input("DNA sequence: ")
+def read_file(filename):
+    with open(filename, 'r') as f:
+        dna = f.read()
+        dna = dna.replace("\n","")
+        return dna
+seq = read_file("dna.txt")
 a, g, c, t = count_dna(seq)
 print("TOTAL COUNT: ", (a+g+t+c), "A: ", a, "G: ", g, "C: ", c, "T: ", t)
