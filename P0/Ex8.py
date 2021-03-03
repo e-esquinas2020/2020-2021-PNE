9f9b3773ec15b84abad2af26edbd19fcf55f3905
e-esquinas2020 <e.esquinas.2020@alumnos.urjc.es>
2021-03-03 13:46:19 +0100

    Corrected

---
+++ P0/Ex8.py
@@ -0,0 +1,11 @@
+#Which is the most frequent base in each gene?
+import Seq0
+
+GENE_FOLDER = "../Session-04/"
+gene_list=["U5", "ADA", "FRAT1", "FXN"]
+
+print("-----| Exercise 8 |------")
+for gene in gene_list:
+    seq = Seq0.seq_read_fasta(GENE_FOLDER + gene + ".txt")
+    inverse = [(value, key) for key, value in Seq0.seq_count(seq).items()]
+    print("Gene", gene, ": Most frequent Base:", max(inverse))
